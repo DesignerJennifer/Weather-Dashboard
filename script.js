4377c83bd31e0649ef35ad3e9e9e97fe147472ea
DesignerJennifer <54449493+DesignerJennifer@users.noreply.github.com>
2020-01-31 07:58:12 -0600

    trying to figure out how to parse data

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 $("#citySearch").on("click", function(event) {
     event.preventDefault();
     
-    var citySrch = $("#citySearch").val().trim();
+var citySrch = $("#citySearch").val().trim();
     cityWeatherSearch(citySrch);
 
 function cityWeatherSearch(currentWeater) {
-
     // Quering OpenWeather api for wether data
     var queryURL = "http://api.openweathermap.org/data/2.5/forecast?" + name + "id=524901&APPID=0d199b71d13983573eefdb14a808d86a";
 
@@ -19,14 +18,15 @@ function cityWeatherSearch(currentWeater) {
         console.log();
 
         var weatherIndex = ["name", "temp", "humidity", "speed", "all"];
-        // var weatherData = JSON.parse(txt);
-        // Constructiong HTML contaning the weather information
-        // var cityName = $("<h3>").text(response.city.id);
-        // var temp = $("<h4>").text(response.main.temp);
-        // var humidity = $("<h4>").text(response.main.humidity);
-        // var windSpeed = $("<h4>").text(response.wind.speed);
-        // var windSpeed = $("<h4>").text(response.wind.speed);
-        // var uvForcast = ("<h4>").text(response.wind.speed);
+        var weatherData = JSON.parse(this.response);
+        
+        // // Constructiong HTML contaning the weather information
+        var cityName = $("<h3>").text(response.city.id);
+        var temp = $("<h4>").text(response.main.temp);
+        var humidity = $("<h4>").text(response.main.humidity);
+        var windSpeed = $("<h4>").text(response.wind.speed);
+        var windSpeed = $("<h4>").text(response.wind.speed);
+        var uvForcast = ("<h4>").text(response.wind.speed);
         document.getElementById("currentWeather-div").innerHTML = weatherIndex[index];
 
         $("#currentWeater-div").empty();
